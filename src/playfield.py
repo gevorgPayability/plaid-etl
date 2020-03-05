0f97d223dfed7d46f6e7924c72d1a9b068242098
gevorgPayability <gevorg@payability.com>
2020-03-05 16:27:45 +0100

    modified extract_accounts_info

--- src/playfield.py
+++ src/playfield.py
@@ -37,11 +37,19 @@ class PlaidJsonExtractor:
         items = self.data['items']
         results = []
         for item in items:
+            item_info = {}
+
+            for element in ['institution_name', 'institution_id', 'item_id']:
+                item_info[element] = item.get(element, None)
+
             for account in item.get('accounts'):
 
                 account_results = {}
-                for element in ["account_id", "mask", "name", "official_name", "type", "subtype"]:
+                for element in ["account_id", "mask", "name", "official_name",
+                                "type", "subtype"]:
                     account_results[element] = account.get(element, None)
+
+                account_results.update(item_info)
 
                 results.append(account_results)
 
@@ -51,8 +59,11 @@ class PlaidJsonExtractor:
 path = "plaid/asset_reports/date=2020-01-02/supplier_key=7f00f2d8-5709-45f6-b4d2-16c952045f1c/c00bc24c-8296-4925-81c4-f0d5029b9a14_2020-01-02T22:25:51Z.json"
 plaid = PlaidJsonExtractor(path=path, bucket_name="payability-datalake")
 
-
 accounts_info = plaid.extract_accounts_info()
+
+accounts_info.columns
+
+accounts_info.head()
 
 
 # transactions = plaid.extract_transactions()
